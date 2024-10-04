2ab3671f9054869713f7e61cc8a0d9506b471662
JeffreyHartman <dracul104@gmail.com>
2024-10-04 14:12:19 -0400

    Forgotten files!

--- mythic2e/randomEvent.py
+++ mythic2e/randomEvent.py
@@ -19,32 +19,34 @@ EVENT_FOCUS_TABLE = [
 
 def main():
     parser = argparse.ArgumentParser(description='Generate a random event.')
-    parser.add_argument('action', nargs='?', default='random_element', help='random_event, focus, action, description, list_element_tables, random_element')
+    parser.add_argument('action', nargs='?', default='npc', help='random_event, focus, action, description, list_element_tables, random_element')
     parser.add_argument('--element', default='noblehouse', help='roll on the specified element table')
     args = parser.parse_args()
 
     if args.action == 'random_event':
-        print(generateRandomEvent())
+        print(generate_random_event())
     elif args.action == 'focus':
-        print('Event Focus: ' + generateEventFocus())
+        print('Event Focus: ' + generate_event_focus())
     elif args.action == 'action':
-        print('Action: ' + generateAction())
+        print('Action: ' + generate_action())
     elif args.action == 'description':
-        print('Description: ' + generateDescriptor())
+        print('Description: ' + generate_descriptor())
     elif args.action == 'list_element_tables':
-        fileList = listElementTables()
+        fileList = list_element_tables()
         print('Element Tables: ' + ', '.join(fileList))
-        #for file in fileList:
-         #   print(file)
     elif args.action == 'random_element':
-        print(getFileHeader(args.element) + ': ' + generateElements(args.element))
+        print(get_file_header(args.element) + ': ' + generate_elements(args.element))
+    elif args.action == 'npc':
+        npc = generate_NPC()
+        for label, value in npc.items():
+            print(f"{label}: {value}")
 
-def generateRandomEvent():
-    eventFocus = generateEventFocus()
-    event = generateAction()
+def generate_random_event():
+    eventFocus = generate_event_focus()
+    event = generate_action()
     return "Event Focus: " + eventFocus + "\nEvent: " + event
 
-def generateEventFocus():
+def generate_event_focus():
     dieRoll = random.randint(1, 100)
     eventFocus = None
 
@@ -55,16 +57,29 @@ def generateEventFocus():
 
     return eventFocus
 
-def generateAction():
-    return getRandomWordFromFile('action1.txt') + ' ' + getRandomWordFromFile('action2.txt')    
+def generate_action():
+    return get_random_word_from_file('action1.txt') + ' ' + get_random_word_from_file('action2.txt')    
 
-def generateDescriptor():
-    return getRandomWordFromFile('descriptor1.txt') + ' ' + getRandomWordFromFile('descriptor2.txt')
+def generate_descriptor():
+    return get_random_word_from_file('descriptor1.txt') + ' ' + get_random_word_from_file('descriptor2.txt')
 
-def generateElements(element):
-    return getRandomWordFromFile('elements/' + element + '.txt') + ' ' + getRandomWordFromFile('elements/' + element + '.txt')
+def generate_elements(element):
+    return generateWordPair('elements/' + element + '.txt')
 
-def listElementTables():
+def generate_NPC():
+    npc = {
+        'Identity': generateWordPair('elements/characteridentity.txt'),
+        'Description': generateWordPair('elements/characterdescription.txt'),
+        'Appearance': generateWordPair('elements/characterapp.txt'),
+        'Background': generateWordPair('elements/characterbackground.txt'),
+        'Personality': generateWordPair('elements/characterpersonality.txt'),
+        'Motivation': generateWordPair('elements/charactermotivation.txt'),
+        'Skills': generateWordPair('elements/characterskill.txt'),
+        'Trait': generateWordPair('elements/charactertrait.txt')
+    }
+    return npc
+
+def list_element_tables():
     # list all the tables in the tables/mythic2e/elements directory
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
@@ -79,7 +94,7 @@ def listElementTables():
 
     return fileList
 
-def getFileHeader(filename):
+def get_file_header(filename):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
     filepath = os.path.join(parent_dir, "tables/mythic2e/elements", filename + '.txt')
@@ -95,7 +110,7 @@ def getFileHeader(filename):
     
     return header
 
-def openFile(filename):
+def open_file(filename):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
     filepath = os.path.join(parent_dir, "tables/mythic2e", filename)
@@ -110,11 +125,14 @@ def openFile(filename):
     
     return content
 
-def getRandomWordFromFile(filename):
-    content = openFile(filename)
+def get_random_word_from_file(filename):
+    content = open_file(filename)
     dieRoll = random.randint(1, len(content))  # Adjust the die roll to the size of the content
     word = content[dieRoll - 1].replace('\n', '')
     return word
+
+def generateWordPair(file):
+    return get_random_word_from_file(file) + ' ' + get_random_word_from_file(file)
 
 if __name__ == "__main__":
     main()
--- mythic2e/scenecheck.py
+++ mythic2e/scenecheck.py
@@ -1,7 +1,45 @@
 import random
+import argparse
+
+SCENE_ADJUSTMENT_TABLE = [
+    (1, "Remove a Character"),
+    (2, "Add a Character"),
+    (3, "Reduce/Remove an Activity"),
+    (4, "Increase an Activity"),
+    (5, "Remove an Object"),
+    (6, "Add an Object"),
+    (7, "Make 2 Adjustments"),
+    (8, "Make 2 Adjustments"),
+    (9, "Make 2 Adjustments"),
+    (10, "Make 2 Adjustments")
+]
 
 def main():
-    print("This is the main function.")
+    parser = argparse.ArgumentParser(description='Generate a random event.')
+    parser.add_argument('action', nargs='?', default='scene_check', help='scene_check|scene_adjustment')
+    parser.add_argument('--chaos', type=int, nargs='?', default='5', help='chaos factor')
+    args = parser.parse_args()
+
+    if args.action == 'scene_check':
+        dieRoll = random.randint(1, 10)
+        print("Scene Check: " + sceneCheck(args.chaos, dieRoll) + " (" + str(dieRoll) + ")")
+    elif args.action == 'scene_adjustment':
+        print("Scene Adjustment: " + sceneAdjustment(args.chaos))
+
+def sceneAdjustment(chaosFactorInt):
+    dieRoll = random.randint(1, 10)
+    for pos, result in SCENE_ADJUSTMENT_TABLE:
+        if dieRoll == pos:
+            return result
+    
+def sceneCheck(chaosFactorInt, dieRoll):
+    if dieRoll <= chaosFactorInt:
+        if dieRoll % 2 == 0:
+            return "Interrupt Scene!"
+        else:
+            return "Altered Scene!"
+    else:
+        return "Normal Scene"
 
 if __name__ == "__main__":
     main()
